229797de70548bae276ee84918068f601d310b8e
Boone <68450612+Expressbadger12@users.noreply.github.com>
2025-10-29 13:20:47 -0700

    Add files via upload

--- FirstPython.py
+++ FirstPython.py
@@ -1,1 +1,9 @@
+
+# print tutorial
+# This is a simple tutorial that uses the print command to describe the syntax of the print command
+
+print("")
+
 print("To print text you write 'print' then inside parenthesis you write what you want to print. If you want to print a string, you'll have to put it in quotes")
+
+print("")
