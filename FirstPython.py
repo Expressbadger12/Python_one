83c8e0eec999766b13afb50814eb644c25f30dfa
Boone <fboonestewart05@gmail.com>
2025-11-03 12:27:47 -0800

    Update FirstPython.py

--- FirstPython.py
+++ FirstPython.py
@@ -5,13 +5,14 @@
 print("")
 
 print("--- Welcome to my Print tutorial! ---")
+
 print("Print is a command that can be used to make the terminal output anything you want")
 
 print("To print text you write 'print' then inside parenthesis you write what you want to print. If you want to print a string, you'll have to put it in quotes")
 
 print("Here is an example of the output of 'print(\"example text\")'")
 
-print("'example text'")
+print("example text")
 
 print("You can also print datastructures that arn't strings like integers by entering them in without quotes such as 'print(4)'")
 
