34edf266c83d2c33c699502e60394641652dc5a6
Boone <fboonestewart05@gmail.com>
2025-11-03 13:55:42 -0700

    Create Practice.py

---
+++ Practice.py
@@ -0,0 +1,24 @@
+#########################################
+# 
+# Author: Boone Stewart
+# Today : 11/3/25
+# Python 1
+#
+# Let's use the print function and have some fun
+
+# Getting to know the print function
+# The print() function takes whatever you give it and shows it in the terminal
+# In this example, we are giving it a string. A string is tezt inside quotes.
+print("Example 1: Hello world!")
+print ("Example 2: My name is Boone and I love music")
+
+###########################################
+
+#Now we can use variables
+#Definition: where you can store information in variables so thatt you can reuse it
+#Le't create some variations
+animal = "whale"
+habitat = "ocean"
+food = "krill"
+
+print("My favorite animal is:", animal)
